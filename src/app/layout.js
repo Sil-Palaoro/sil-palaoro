a98a2c31a9ed4c4bafaae5be08239cc93ae7219c
Sil_Palaoro <silvinapalaoro@gmail.com>
2024-01-14 12:22:52 -0300

    rename sim-movies

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function LayoutHome({ children }) {
   return (
     <html lang="es">
       <head>
-        <link rel="icon" href="/check-icon.png" />
+        
         <link
           href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@1,600;1,700&family=Josefin+Slab:wght@500&family=Lato:wght@700&family=Merriweather:wght@300&family=Poppins:ital,wght@1,600&display=swap"
           rel="stylesheet"
