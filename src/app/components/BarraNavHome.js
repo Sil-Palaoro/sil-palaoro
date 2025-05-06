1fcf8936648180d70dc36ca486a8e41077079249
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-05-06 19:31:45 -0300

    Added hamburger menu. Still not working, but CSS ready

--- src/app/components/BarraNavHome.js
+++ src/app/components/BarraNavHome.js
@@ -13,13 +13,19 @@ function BarraNavHome () {
   const pathname = usePathname();
   const { language, changeLanguage } = useLanguage();
   const { t, i18n } = useTranslation();
-  
+  // const hamMenuRef = useRef(null);
+  // const hamNavRef = useRef(null);
+  const [hamMenuButtonVar, setHamMenuButtonVar] = useState(null);
+  const [offScreenMenuVar, setOffScreenMenuVar] = useState(null);  
 
   console.log("BarraNavHome language:", language); // Debugging
 
  
   useEffect(() => {
     setIsClient(true)
+
+    setHamMenuButtonVar(document.getElementById('hamMenuButton'));
+    setOffScreenMenuVar(document.getElementById('offScreenMenu'));
   }, [])
  
   return (
@@ -51,12 +57,73 @@ function BarraNavHome () {
             <button onClick={() => {
               console.log("Language in button in BarraNavHome:", language);
               changeLanguage(language === 'es' ? 'en' : 'es');
-            }} className={styles.button}>
+            }} className={styles.languageButton}>
                 {language === 'es' ? "Change to EN" : "Cambiar a ES"}
             </button>
           </li>          
-         </ul>
+        </ul>
+
+        {/*Menu hamburguesa*/}
+        <div className={styles.ul} id={styles.menuBar}>
+        {/* <div className={styles.hamNav} id={styles.menu}> */}
+          <button 
+          className={styles.hamMenuButton}
+          id={styles.hamMenuButton} 
+          // ref={hamMenuRef}
+          onClick={ () => {
+            // hamMenuRef.current.classList.toggle('active');
+            // hamNavRef.current.classList.toggle('active');
+            // const hamMenuButtonVar = document.getElementById('hamMenuButton');
+            // const offScreenMenuVar = document.getElementById('offScreenMenu');
+
+            console.log("hamMenuButtonVar:", hamMenuButtonVar);
+            console.log("offScreenMenuVar:", offScreenMenuVar);
+
+            if (hamMenuButtonVar && offScreenMenuVar) {
+              hamMenuButtonVar.classList.toggle('active');
+              offScreenMenuVar.classList.toggle('active');
+            } else {
+              console.error("Elements not found!");
+            }
+
+            // hamMenuButtonVar.classList.toggle('active');
+            // offScreenMenuVar.classList.toggle('active');
+          }}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        {/* </div> */}
+        </div>
       </nav>
+
+      {/*Links Menu hamburguesa*/}   
+
+      <div className={styles.offScreenMenu} id={styles.offScreenMenu}>
+        <ul className={styles.ulHam}>          
+          <li className={styles.liHam}>
+            <Link title="Home" className={styles.a} href={`/${language}`}>Home</Link>
+          </li>          
+          <li className={styles.liHam}>
+            <Link href={`/${language}#projects`} title="Projects" className={styles.a}>{t("tituloProjects")}</Link>
+          </li>
+          <li className={styles.liHam}>
+            <Link href={`/${language}#skills`} title="Skills" className={styles.a}>{t("skills")}</Link>
+          </li>
+          <li className={styles.liHam}>
+            <Link href={`/${language}#about`} title="About" className={styles.a}>{t("tituloAbout")}</Link>
+          </li>
+          <li className={styles.liHam}>
+            <button onClick={() => {
+              console.log("Language in button in BarraNavHome:", language);
+              changeLanguage(language === 'es' ? 'en' : 'es');
+            }} className={styles.button}>
+                {language === 'es' ? "Change to EN" : "Cambiar a ES"}
+            </button>
+          </li>       
+        </ul>
+      </div>        
     </header>
     )}
     </>
